660a346df8b554e17c435817cb2841bc1d6c663a
jj <18720930840@163.com>
2017-10-18 17:53:13 +0800

    创建选择联系人完成
    Signed-off-by: jj <18720930840@163.com>

--- js/shop.js
+++ js/shop.js
@@ -248,12 +248,16 @@ var Shop = (function() {
 				});
 		};
 		
-		this.MakeOrder = function(productId,quantity,peopleAmount,address,phone,remark,bookedDate,categoryId,onSuccess,onError){
+		this.MakeOrder = function(productId,quantity,peopleAmount,address,phone,remark,bookedDate,categoryId,linkMains,onSuccess,onError){
 			if(!_getNetwork()) {
 				mui.toast("没有网络连接，请稍后再试！");
 				return;
 			}
-			PostRequest(API_SHOP+'MakeOrder?productId='+productId+'&quantity='+quantity+'&peopleAmount='+peopleAmount+'&address='+address+'&phone='+phone+'&remark='+remark+'&bookedDate='+bookedDate+'&categoryId='+categoryId,'',function(result){
+			var LinkManID = [];
+			for(var i = 0; i < linkMains.length; i++) {
+				LinkManID.push(linkMains[i].Id+"");
+			}
+			PostRequest(API_SHOP+'MakeOrder?productId='+productId+'&quantity='+quantity+'&peopleAmount='+peopleAmount+'&address='+address+'&phone='+phone+'&remark='+remark+'&bookedDate='+bookedDate+'&categoryId='+categoryId,'LinkManID',function(result){
 				if (onSuccess) {
 					onSuccess.call(this,result);
 				}
